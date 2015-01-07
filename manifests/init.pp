3123536a7355c82afa4e70666fd093ce13c188a1
Tim Taylor <tim@tool-man.org>
2015-01-07 01:19:24 -0800

    tap caskroom/fonts

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,7 @@ class brewcask {
   $cask_room = "${cask_home}/Caskroom"
 
   homebrew::tap { 'caskroom/cask': }
+  homebrew::tap { 'caskroom/fonts': }
 
   file { $cask_home:
     ensure => directory
@@ -26,7 +27,7 @@ class brewcask {
   }
 
   package { 'brew-cask':
-    require  => Homebrew::Tap['caskroom/cask'],
+    require  => Homebrew::Tap['caskroom/cask','caskroom/fonts'],
     provider => homebrew
   }
 
