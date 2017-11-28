b1998069ec3726241a4a7628cba907c108c40bee
n0ts <me@n0ts.org>
2017-11-27 21:40:33 -0800

    Add provider test

---
+++ spec/unit/provider/package/brewcask_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+provider = Puppet::Type.type(:package).provider(:brewcask)
+
+describe provider do
+  it 'should have an install method' do
+    is_expected.to respond_to(:install)
+  end
+
+  it 'should have a uninstall method' do
+    is_expected.to respond_to(:uninstall)
+  end
+
+  it 'should not have a latst method' do
+    is_expected.not_to respond_to(:latest)
+  end
+
+  it 'should not have a update method' do
+    is_expected.not_to respond_to(:update)
+  end
+
+  describe 'when installing' do
+    context 'installing cask without install options' do
+      before do
+        @resource = Puppet::Type.type(:package).new(
+                                                    :name     => 'cask',
+                                                    :ensure   => :present,
+                                                    :provider => :brewcask,
+                                                   )
+        @provider = provider.new(@resource)
+      end
+
+      it 'should return install command' do
+        expect{ @provider.install }.to raise_error(Puppet::ExecutionFailure,
+                                                    /brew boxen-cask-install cask/)
+      end
+
+      it 'should return nil query' do
+        expect(@provider.query).to eq(nil)
+      end
+    end
+
+    context 'installing cask with install options' do
+      before do
+        @resource = Puppet::Type.type(:package).new(
+                                                    :name     => 'cask',
+                                                    :ensure   => :present,
+                                                    :provider => :brewcask,
+                                                    :install_options => '--foo',
+                                                   )
+        @provider = provider.new(@resource)
+      end
+
+      it 'should return install command with install options' do
+        expect { @provider.install }.to raise_error(Puppet::ExecutionFailure,
+                                                    /brew cask install --foo cask/)
+      end
+    end
+  end
+
+  describe 'when uninstalling' do
+      before do
+        @resource = Puppet::Type.type(:package).new(
+                                                    :name     => 'cask',
+                                                    :ensure   => :present,
+                                                    :provider => :brewcask,
+                                                   )
+        @provider = provider.new(@resource)
+      end
+
+      it 'should return uninstall command' do
+        expect { @provider.uninstall }.to raise_error(Puppet::ExecutionFailure,
+                                                      /brew cask uninstall --force cask/)
+      end
+  end
+end
